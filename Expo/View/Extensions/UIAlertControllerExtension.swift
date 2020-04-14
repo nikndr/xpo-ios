631f6e37375b39c735980eb020a1f3930f070756
TheGreatNectarine <margal.nik@gmail.com>
2020-04-14 19:12:30 +0300

    Assigned server capabilities: auth; refactored app logic after sign up

---
+++ Expo/View/Extensions/UIAlertControllerExtension.swift
@@ -0,0 +1,9 @@
+//
+//  UIAlertControllerExtension.swift
+//  Expo
+//
+//  Created by Nikandr Marhal on 14.04.2020.
+//  Copyright © 2020 Nikandr Marhal. All rights reserved.
+//
+
+import Foundation
